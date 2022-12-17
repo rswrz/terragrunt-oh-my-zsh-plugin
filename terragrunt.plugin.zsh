f383a0960fa8d186a0851974e871db94622bcba4
rswrz <git@rswrz.net>
2022-12-17 23:56:44 +0100

    Add additional aliases

--- terragrunt.plugin.zsh
+++ terragrunt.plugin.zsh
@@ -10,3 +10,9 @@ function tg_prompt_info() {
 
 # alias
 alias tg="terragrunt"
+alias tgi="terragrunt init"
+alias tgp="terragrunt plan"
+alias tga="terragrunt apply"
+alias tgai="terragrunt run-all init"
+alias tgap="terragrunt run-all plan"
+alias tgaa="terragrunt run-all apply"
